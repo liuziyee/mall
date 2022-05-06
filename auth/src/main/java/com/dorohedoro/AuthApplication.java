f016e9aa62b3661a52cb933f34d5d31ae19f261d
liuziyee <liuziyee1994@163.com>
2022-05-06 21:41:59 +0800

    :building_construction:

--- auth/src/main/java/com/dorohedoro/AuthApplication.java
+++ auth/src/main/java/com/dorohedoro/AuthApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //@EnableJpaAuditing
 @EnableDiscoveryClient
 @SpringBootApplication
-@MapperScan("com.dorohedoro.mapper")
+//@MapperScan("com.dorohedoro.mapper")
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
--- auth/src/test/java/com/dorohedoro/unit/MybatisPlusTest.java
+++ auth/src/test/java/com/dorohedoro/unit/MybatisPlusTest.java
@@ -1,10 +1,15 @@
 package com.dorohedoro.unit;
 
 import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.dorohedoro.entity.User;
 import com.dorohedoro.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +34,55 @@ public class MybatisPlusTest {
     
     @Test
     public void selectOne() {
+        String username = "yee";
         QueryWrapper<User> wrapper = new QueryWrapper<>();
-        wrapper.like("username", "yee");
+        wrapper.like(!StringUtils.isBlank(username), "username", username)
+                .and(i -> i.isNotNull("extra_info").or().isNotNull("password"));
         User user = userMapper.selectOne(wrapper);
         log.info("user data: {}", JSON.toJSONString(user));
     }
+    
+    @Test
+    public void deleteOne() {
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq("username", "liuziyee");
+        userMapper.delete(wrapper);
+    }
+    
+    @Test
+    public void updateOne() {
+        QueryWrapper<User> wrapper = new QueryWrapper<>();
+        wrapper.eq("username", "liuziyee");
+
+        User user = new User();
+        user.setUsername("liuye");
+        userMapper.update(user, wrapper);
+    }
+    
+    @Test
+    public void selectOneLambda() {
+        LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(User::getUsername, "liuziye");
+
+        User user = userMapper.selectOne(wrapper);
+        log.info("user data: {}", JSON.toJSONString(user));
+    }
+    
+    @Test
+    public void updateOneLambda() {
+        LambdaUpdateWrapper<User> wrapper = new LambdaUpdateWrapper<>();
+        wrapper.eq(User::getUsername, "liuye");
+        wrapper.set(User::getUsername, "liuziyee");
+
+        userMapper.update(null, wrapper);
+    }
+    
+    @Test
+    public void selectPage() {
+        Page<User> page = new Page<>(1, 10);
+
+        userMapper.selectPage(page, null);
+        log.info("page data: {}", JSON.toJSONString(page));
+    }
+    
 }
